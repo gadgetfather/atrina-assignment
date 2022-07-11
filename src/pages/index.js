747e6a44ad27898de84275d5b51f0a44aff43486
Akshay Kamble <kamblea635@gmail.com>
2022-07-11 16:19:48 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,5 @@
 export { Homepage } from "./Homepage/Homepage";
+export { ProductPage } from "./ProductPage/ProductPage";
+export { LoginPage } from "./LoginPage/LoginPage";
+export { SignupPage } from "./SignupPage/SignupPage";
+export { CartPage } from "./CartPage/CartPage";
