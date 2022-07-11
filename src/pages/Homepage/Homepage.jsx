3c27cc0baf0ac5a53ed83537552e0c3676a14c5d
Akshay Kamble <kamblea635@gmail.com>
2022-07-11 13:48:49 +0530

    created HomePage

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Hero, ProductCard } from "../../components";
+import styles from "./Homepage.module.css";
+export default function Homepage() {
+  return (
+    <div className={styles.homepage_container}>
+      <Hero />
+      <div className={styles.our_products}>
+        <h2 className={styles.section_title}>Some Products</h2>
+        <div className={styles.product_section}>
+          <ProductCard />
+        </div>
+      </div>
+    </div>
+  );
+}
