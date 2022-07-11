8ddff6c2ad3754e9e8dbcf5eca88de027f760192
Akshay Kamble <kamblea635@gmail.com>
2022-07-11 13:49:32 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./App.css";
-
+import { Navbar, Sidebar } from "./components";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <div className="page_container">
+        <Navbar setIsOpen={setIsOpen} isOpen={isOpen} />
+        <Sidebar setIsOpen={setIsOpen} isOpen={isOpen} />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
