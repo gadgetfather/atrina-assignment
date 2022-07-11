24196f9cc13c0a9f65f2251c8a658e5c89ee7bfa
Akshay Kamble <kamblea635@gmail.com>
2022-07-11 13:49:01 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Navbar } from "./Navbar/Navbar";
+export { Sidebar } from "./Sidebar/Sidebar";
+export { Hero } from "./Hero/Hero";
+export { ProductCard } from "./ProductCard/ProductCard";
