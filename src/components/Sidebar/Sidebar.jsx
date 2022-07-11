ef95e9f53033637aba447b75f7394055ef1b5450
Akshay Kamble <kamblea635@gmail.com>
2022-07-11 18:11:00 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { Inventory, Login, MakiClothingStore } from "../../utility/Icons";
+import {
+  IcRoundHome,
+  IcRoundShoppingCart,
+  Inventory,
+  Login,
+  MakiClothingStore,
+} from "../../utility/Icons";
+import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 export function Sidebar(props) {
   const { isOpen } = props;
 
   return (
     <div className={`${styles.sidebar} ${isOpen && styles.sidebar_open} `}>
-      <div className={styles.sidebar_item}>
+      <Link to={"/"} className={styles.sidebar_item}>
+        <IcRoundHome />
+        <p>Home</p>
+      </Link>
+      <Link to={"/ProductPage"} className={styles.sidebar_item}>
         <MakiClothingStore />
         <p>Products</p>
-      </div>
-      <div className={styles.sidebar_item}>
+      </Link>
+      <Link to={"/cart"} className={styles.sidebar_item}>
+        <IcRoundShoppingCart />
+        <p>Cart</p>
+      </Link>
+      <Link to={"/inventory"} className={styles.sidebar_item}>
         <Inventory />
         <p>Inventory</p>
-      </div>
-      <div className={styles.sidebar_item}>
+      </Link>
+      <Link to={"/LoginPage"} className={styles.sidebar_item}>
         <Login />
         <p>Login</p>
-      </div>
+      </Link>
     </div>
   );
 }
