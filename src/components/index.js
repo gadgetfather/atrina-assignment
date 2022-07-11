c8317d4bca8f64f16dc6c0ff80ecef48ea3a8385
Akshay Kamble <kamblea635@gmail.com>
2022-07-11 16:19:15 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar/Navbar";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { Hero } from "./Hero/Hero";
 export { ProductCard } from "./ProductCard/ProductCard";
+export { CartCard } from "./CartCard/CartCard";
