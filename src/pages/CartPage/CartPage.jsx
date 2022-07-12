bf8ee61bccba6080add24fc15351d00557302114
Akshay Kamble <kamblea635@gmail.com>
2022-07-12 12:36:54 +0530

    created cart-page

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,35 +1,49 @@
 import React from "react";
 import { CartCard } from "../../components";
+import { useCart } from "../../context/cart-context";
 import styles from "./CartPage.module.css";
 export function CartPage() {
+  const {
+    cart: { cart },
+  } = useCart();
+
   return (
     <div className={styles.cart_page}>
       <h1 className={styles.section_title}>Cart</h1>
-      <div className={styles.main_content}>
-        <div className={styles.left_section}>
-          <CartCard />
-        </div>
-        <div className={styles.right_section}>
-          <div className={styles.bill_container}>
-            <h2>Total Order</h2>
-            <div className={styles.items_container}>
-              <div className={styles.item}>
-                <span>Item</span> <span>Rs.100</span>
+      {cart.length === 0 ? (
+        <h2 className={styles.section_title}>Cart is empty..</h2>
+      ) : (
+        <div className={styles.main_content}>
+          <div className={styles.left_section}>
+            {cart.map((product) => (
+              <CartCard key={product.data_id} {...product} />
+            ))}
+          </div>
+          <div className={styles.right_section}>
+            <div className={styles.bill_container}>
+              <h2>Total Order</h2>
+              <div className={styles.items_container}>
+                {cart.map((item, i) => (
+                  <div key={i} className={styles.item}>
+                    <span>{item.title}</span>{" "}
+                    <span>{item.price * item.quantity}</span>
+                  </div>
+                ))}
               </div>
-              <div className={styles.item}>
-                <span>Item</span> <span>Rs.100</span>
+              <div className={styles.total_amount}>
+                <span>Total Amount</span>{" "}
+                <span>
+                  {cart.reduce(
+                    (prev, acc) => acc.price * acc.quantity + prev,
+                    0
+                  )}
+                </span>
               </div>
-              <div className={styles.item}>
-                <span>Item</span> <span>Rs.100</span>
-              </div>
+              <button className={styles.pay_button}>Pay Now</button>
             </div>
-            <div className={styles.total_amount}>
-              <span>Total Amount</span> <span>Rs1000</span>
-            </div>
-            <button className={styles.pay_button}>Pay Now</button>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
