7cdf9e8a9942f3f63064f3d3bda7488229055beb
Akshay Kamble <kamblea635@gmail.com>
2022-07-12 12:37:32 +0530

    mapped products on productpage

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useInsertionEffect } from "react";
 import { ProductCard } from "../../components";
+import { useInventory } from "../../context/inventory-context";
+
 import styles from "./ProductPage.module.css";
 export function ProductPage() {
+  const { inventory } = useInventory();
   return (
     <div className={styles.ProductPage}>
       <h1 className={styles.section_title}>Products</h1>
       <div className={styles.product_grid}>
-        {[1, 2, 3, 4, 5, 6].map((product) => (
-          <ProductCard />
+        {inventory.map((product) => (
+          <ProductCard key={product.data_id} {...product} />
         ))}
       </div>
     </div>
