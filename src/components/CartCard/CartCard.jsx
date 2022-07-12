d59d8fafe9c6389c6db861e3f86279ce48c8e219
Akshay Kamble <kamblea635@gmail.com>
2022-07-12 12:34:09 +0530

    cart quantity functionality added

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,27 +1,55 @@
 import React from "react";
+import { useCart } from "../../context/cart-context";
+import {
+  DECREASE_QUANTITY,
+  INCREASE_QUANTITY,
+  REMOVE_FROM_CART,
+} from "../../reducer/cartReducer";
 import { IcSharpClose } from "../../utility/Icons";
 import styles from "./CartCard.module.css";
-export function CartCard() {
+export function CartCard(props) {
+  const { data_id, title, image, price, quantity = 0 } = props;
+  const { dispatch } = useCart();
+  const handleRemoveFromCart = (data_id) => {
+    dispatch({ type: REMOVE_FROM_CART, payload: data_id });
+  };
+  const handleIncreaseQuantity = (data_id) => {
+    console.log("clicked");
+    dispatch({ type: INCREASE_QUANTITY, payload: data_id });
+  };
+  const handleDecreaseQuantity = (data_id) => {
+    console.log("clicked");
+    dispatch({ type: DECREASE_QUANTITY, payload: data_id });
+  };
   return (
     <div className={styles.card_container}>
-      <IcSharpClose className={styles.close_icon} />
+      <IcSharpClose
+        className={styles.close_icon}
+        onClick={() => handleRemoveFromCart(data_id)}
+      />
       <div className={styles.image_container}>
-        <img
-          className={styles.product_image}
-          src="https://picsum.photos/200/300"
-          alt=""
-        />
+        <img className={styles.product_image} src={image} alt="" />
       </div>
       <div className={styles.right_section}>
-        <h2>Product Title</h2>
+        <h2>{title}</h2>
         <div className={styles.modify_quantity}>
           <h4>Quantity</h4>
-          <button className={styles.quantity_button}>-</button>
-          <p>0</p>
-          <button className={styles.quantity_button}>+</button>
+          <button
+            onClick={() => handleDecreaseQuantity(data_id)}
+            className={styles.quantity_button}
+          >
+            -
+          </button>
+          <p>{quantity}</p>
+          <button
+            onClick={() => handleIncreaseQuantity(data_id)}
+            className={styles.quantity_button}
+          >
+            +
+          </button>
         </div>
         <div>
-          <p>Price</p>
+          <p>Rs.{price}</p>
         </div>
       </div>
     </div>
