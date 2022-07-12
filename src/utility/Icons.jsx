f31237bdec76f62418a7144d56bb333138a191ba
Akshay Kamble <kamblea635@gmail.com>
2022-07-12 17:40:54 +0530

    Update Icons.jsx

--- src/utility/Icons.jsx
+++ src/utility/Icons.jsx
@@ -159,3 +159,21 @@ export function IcRoundDelete(props) {
     </svg>
   );
 }
+
+export function MaterialSymbolsClose(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6Z"
+      ></path>
+    </svg>
+  );
+}
