22509e1f9eadf589c4b5c103e41efdf38ec3df46
Akshay Kamble <kamblea635@gmail.com>
2022-07-12 12:37:17 +0530

    mapped inventory

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { InventoryCard, ProductForm } from "../../components";
+import { EditModal, InventoryCard, ProductForm } from "../../components";
+
+import { useInventory } from "../../context/inventory-context";
 import styles from "./InventoryPage.module.css";
 export function InventoryPage() {
+  const { inventory } = useInventory();
+  const { editModal } = useInventory();
   return (
     <div className={styles.InventoryPage}>
+      {editModal && <EditModal />}
       <h2 className={styles.section_title}>Inventory</h2>
       <div className={styles.inventory_section}>
         <div className={styles.left_section}>
-          <InventoryCard />
-          <InventoryCard />
-          <InventoryCard />
-          <InventoryCard />
-          <InventoryCard />
+          {inventory.map((product) => (
+            <InventoryCard key={product.data_id} {...product} />
+          ))}
         </div>
         <div className={styles.right_section}>
           <ProductForm />
